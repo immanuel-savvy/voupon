66175750d73ef2b9b40de909e640adc2c8e1886b
“immanuel-savvy” <immanuelsavvy@gmail.com>
2023-02-28 15:16:11 +0100

    1677593771715813341

--- src/assets/js/utils/constants.js
+++ src/assets/js/utils/constants.js
@@ -2,9 +2,7 @@ const DEV = false;
 
 const hostname = DEV ? "http://localhost" : "https://voupon.udaralinksapp.com";
 
-const client_domain = DEV
-  ? `${hostname}:3000`
-  : `https://magenta-sprinkles-e1e108.netlify.app`;
+const client_domain = DEV ? `${hostname}:3000` : `https://voupon.netlify.app/`;
 
 const domain = DEV ? `${hostname}:1448` : `https://voupon.udaralinksapp.com`;
 
